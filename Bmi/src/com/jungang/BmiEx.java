9e264b8b1058422368cb5d6830f0f040dc43efe7
user <user@DESKTOP-2PGRMF4>
2023-03-17 12:37:48 +0900

    내 프로젝트 추가

---
+++ Bmi/src/com/jungang/BmiEx.java
@@ -0,0 +1,17 @@
+package com.jungang;
+
+public class BmiEx {
+
+	public static void main(String[] args) {
+		String name = "김지수";
+		int age = 25;
+		String id = "qqq8128";
+		double height =1.6;
+		int money = 10000000;
+		
+		System.out.println(money);
+		
+		
+	}
+
+}
