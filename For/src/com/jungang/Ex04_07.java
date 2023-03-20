c897bc2edbbdd52c03d76df8a749e90a50c0b3f8
user <user@192.168.123.100>
2023-03-20 20:04:50 +0900

    03월 20일_ 예금,출금,잔고,종료 코드 집에서 수정

--- For/src/com/jungang/Ex04_07.java
+++ For/src/com/jungang/Ex04_07.java
@@ -1,31 +1,42 @@
 package com.jungang;
 import java.util.Scanner;
 public class Ex04_07 {
-
+//1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int money = 0;
+		int num = 0;
 		
+		Outter:
 		while(true) {
-	
 			System.out.println("=================================");
-			System.out.println("1: 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
+			System.out.println("1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료");
 			System.out.println("=================================");
-			//사용자의 입력을 int 타입으로 반환
+			
 			System.out.print("선택> ");
 			int user = scan.nextInt();
-		
-			switch(user) {
-				case 1:
-					//반복문, switch 문
-					break;
-				case 2:
-					break;
-				case 3:
-					break;
-				case 4:
-					
+			
+			switch (user) {
+			case 1:
+				System.out.print("예금액> ");
+				money += scan.nextInt();
+				System.out.println("사용자 입력: "+ money);
+				break;
+			case 2:
+				System.out.print("출금액> ");
+				num += scan.nextInt();
+				System.out.println("사용자 입력: " + num);
+				break;
+			case 3:
+				System.out.print("잔고> ");
+				System.out.println(money-num);
+				break;
+			case 4:
+				System.out.println("프로그램 종료");
+				break Outter;
 			}
+			
 		}
+		
 	}	
 }
