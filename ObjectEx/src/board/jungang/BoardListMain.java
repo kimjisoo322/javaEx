3a9f4515df5131996c7ae51ae9eb055f556839b2
user <user@DESKTOP-2PGRMF4>
2023-03-22 18:45:56 +0900

    03월 22일 추가 수정

--- ObjectEx/src/board/jungang/BoardListMain.java
+++ ObjectEx/src/board/jungang/BoardListMain.java
@@ -31,6 +31,7 @@ public class BoardListMain {
 		}
 		*/
 		//참조타입: String처럼 Board타입이 온거임! 
+		/*
 		Board[] board = new Board[5];
 		
 		board[0] = new Board("게시글1", "내용1", "");
@@ -38,10 +39,25 @@ public class BoardListMain {
 		board[2] = new Board("게시글3", "내용3");
 		board[3] = new Board("게시글4", "내용4");
 		board[4] = new Board("게시글5", "내용5");
+		*/
+		
+		Board[] board = new Board[5];
+		board[0] = new Board("게시글1","내용1");
+		board[1] = new Board("게시글2","내용2");
+		board[2] = new Board("게시글3","내용3","김지수","23-03-22",0);
+		board[3] = new Board("게시글4","내용4","윤시윤");
+		board[4] = new Board("게시글5","내용5");
 		
 		
-		
-		
+		for(Board store:board) {
+			System.out.println("========================");
+			System.out.println(store.content);
+			System.out.println(store.title);
+			System.out.println(store.writer);
+			System.out.println(store.date);
+			System.out.println(store.count);
+			
+		}
 		
 		
 	}
